ea13d574ad4176cb2aa8aac915fbe19aa191e60c
eduardpetraeus <clauspetraeus@hotmail.com>
2020-05-13 20:45:29 +0200

    no message

---
+++ Datawarehouse/Database salg/Archive/Order_Products__Train.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE [Archive].[Order_Products__Train]
+(
+    [Id_Order_Products__Train]   BIGINT       NOT NULL,
+	[Order_Id_Train]             INT          NOT NULL,
+	[Product_Id_Train]           INT          NOT NULL,
+	[Add_To_Cart_Order_Train]    INT          NOT NULL,
+	[Reordered_Train]            INT          NOT NULL,
+                                 
+	/* Metadata */               
+    [Meta_Id]                    BIGINT       NOT NULL IDENTITY (1, 1),
+    [Meta_VersionNo]             INT          NOT NULL,
+    [Meta_ValidFrom]             DATETIME     NOT NULL,
+    [Meta_ValidTo]               DATETIME     NULL,
+    [Meta_IsValid]               BIT          NOT NULL, -- If false rows exists in data validation table
+    [Meta_IsCurrent]             BIT          NOT NULL,
+    [Meta_IsDeleted]             BIT          NOT NULL,
+    [Meta_CreateTime]            DATETIME     NOT NULL,
+    [Meta_CreateJob]             BIGINT       NOT NULL, -- Reference to the audit framework
+    [Meta_UpdateTime]            DATETIME     NULL,
+    [Meta_UpdateJob]             BIGINT       NULL, -- Reference to the audit framework
+    [Meta_DeleteTime]            DATETIME     NULL,
+    [Meta_DeleteJob]             BIGINT       NULL, -- Reference to the audit framework
+          /* Constraints */
+	CONSTRAINT PK_Archive_Order_Products__Train PRIMARY KEY CLUSTERED ([Id_Order_Products__Train], [Meta_ValidFrom])
+);
+GO
+	
+CREATE UNIQUE INDEX IDX_Archive_Order_Products__Train ON [Archive].[Order_Products__Train] ([Meta_Id])
---
+++ Datawarehouse/Database salg/Extract/Order_Products__Train.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [Extract].[Order_Products__Train]
+(   
+    [Id_Order_Products__Train]       BIGINT          NOT NULL,
+	[Order_Id_Train]                 INT             NOT NULL,
+	[Product_Id_Train]               INT             NOT NULL,
+	[Add_To_Cart_Order_Train]        INT             NOT NULL,
+	[Reordered_Train]                INT             NOT NULL,
+
+
+	/* Metadata */
+	[Meta_Id]           BIGINT   IDENTITY (1,1)      NOT NULL,
+	[Meta_CreateTime]   DATETIME DEFAULT GETDATE()   NOT NULL,
+	[Meta_CreateJob]	BIGINT		                 NOT NULL
+
+	CONSTRAINT PK_Order_Products__Train_Id PRIMARY KEY CLUSTERED ([Meta_Id])
+)
+GO
+
+CREATE UNIQUE INDEX IDX_Order_Products__Train
+ON [Extract].[Order_Products__Train] ([Id_Order_Products__Train], [Meta_CreateTime])
