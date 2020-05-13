5dd27c950f7241cd6fa5957f7350dd610a37f5eb
eduardpetraeus <clauspetraeus@hotmail.com>
2020-05-13 13:32:00 +0200

    Inspirations kode

---
+++ Inspirations kode/sp_Data_Dim_Tid.sql
@@ -0,0 +1,217 @@
+﻿CREATE PROCEDURE [Staging].[sp_Data_Dim_Tid]
+
+	 @Dato DATETIME
+    ,@Slut_Dato DATETIME
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    -- Set første dag til mandag.
+    SET DATEFIRST 1
+
+	DECLARE @Dato_Kode INT
+	DECLARE @Aarstal INT
+	DECLARE @Aar_Uge INT
+	DECLARE @Kvartal INT
+	DECLARE @Kvartal_Uge INT
+	DECLARE @Maaned	INT
+	DECLARE @Uge INT
+	DECLARE @Dag_Uge INT
+	DECLARE @Dag_Aar INT
+	DECLARE @Dag_Maaned	INT
+	DECLARE @Maaned_Navn NVARCHAR (20) 
+	DECLARE @Dag_Navn NVARCHAR (20)       
+    DECLARE	@Skudaar BIT
+    
+	-- Variables bruges ikke til insert i tabel, kun i ST.
+	DECLARE @IsLastDayInMonth BIT
+	DECLARE	@DaysFromPalmSunday BIGINT
+    DECLARE @PalmSunday DATETIME
+    DECLARE @ThursdayInWeek DATETIME
+    DECLARE @Sidste_Aar INT = NULL
+
+    -- Bygg kalender
+    WHILE (@Dato <= @Slut_Dato)
+    BEGIN
+
+        /* -------------------------------------- *\
+        If year changed recalculate palm Sunday
+        http://www.assa.org.au/edm.html#Computer
+        \* -------------------------------------- */
+        IF @Sidste_Aar IS NULL OR @Sidste_Aar <> DATEPART(YEAR, @Dato)
+        BEGIN
+	        -- Intermediate results
+	        DECLARE @FirstDig bigint
+	        DECLARE @Remain19 bigint
+	        DECLARE @temp bigint 
+	   
+	        -- Table A to E results
+	        DECLARE @tA bigint
+	        DECLARE @tB bigint
+	        DECLARE @tC bigint
+	        DECLARE @tD bigint
+	        DECLARE @tE bigint        
+	   
+	        DECLARE @d bigint
+	        DECLARE @m bigint  
+	   
+            -- Calculate Paschal Full Moon (PFM) date
+	        SET @FirstDig = DATEPART(YEAR, @Dato) / 100 -- First 2 digits of year
+	        SET @Remain19 = DATEPART(YEAR, @Dato) % 19  -- Remainder of year / 19
+
+	        SET @temp = (@FirstDig - 15) / 2 + 202 - 11 * @Remain19    
+	    
+	        SET @temp = CASE @FirstDig 
+				WHEN 21 THEN @temp - 1
+				WHEN 24 THEN @temp - 1
+				WHEN 25 THEN @temp - 1
+				WHEN 27 THEN @temp - 1
+				WHEN 28 THEN @temp - 1
+				WHEN 29 THEN @temp - 1
+				WHEN 30 THEN @temp - 1
+				WHEN 31 THEN @temp - 1
+				WHEN 32 THEN @temp - 1
+				WHEN 34 THEN @temp - 1
+				WHEN 35 THEN @temp - 1
+				WHEN 38 THEN @temp - 1
+				WHEN 33 THEN @temp - 2
+				WHEN 36 THEN @temp - 2
+				WHEN 37 THEN @temp - 2
+				WHEN 39 THEN @temp - 2
+				WHEN 40 THEN @temp - 2
+				ELSE @temp
+			END    
+	   
+	        SET @temp = @temp % 30
+
+	        SET @tA = @temp + 21
+		    IF (@temp = 29)
+                SET @tA = @tA - 1
+		    IF (@temp = 28 And @Remain19 > 10)
+                SET @tA = @tA - 1
+	   
+	        -- Find the next Sunday
+	        SET @tB = (@tA - 19) % 7
+	    
+	        SET @tC = (40 - @FirstDig) % 4
+		    IF (@tC = 3)  
+			    SET @tC = @tC + 1
+		    IF (@tC > 1)  
+			    SET @tC = @tC + 1
+	       
+	        SET @temp = DATEPART(YEAR, @Dato) % 100
+	        SET @tD = (@temp + @temp / 4) % 7
+	    
+	        SET @tE = ((20 - @tB - @tC - @tD) % 7) + 1
+	        SET @d = @tA + @tE
+
+	        -- Return the date
+	        IF (@d > 31) 
+	        BEGIN
+		      SET @d = @d - 31
+		      SET @m = 4
+	        END
+	        ELSE
+	        BEGIN
+		      SET @m = 3
+	        END
+
+	        SET @PalmSunday = CONVERT(DATETIME, CONVERT(NVARCHAR, DATEPART(YEAR, @Dato)) +
+                                                RIGHT('0' + CONVERT(NVARCHAR, @m), 2) + 
+                                                RIGHT('0' + CONVERT(NVARCHAR, @d), 2), 112) - 7
+        END
+        /* -------------------------------------- *\
+        DONE!
+        \* -------------------------------------- */
+
+		SET  @ThursdayInWeek = @Dato - DATEPART(DW, @Dato) + 1 + 3	
+
+		-- Date key
+	    SET @Dato_Kode = DATEPART (YEAR, @Dato) * 10000 +
+	                   DATEPART (MONTH, @Dato) * 100 +
+	                   DATEPART (DAY, @Dato)
+		
+		-- Get vital date informations
+		
+		SET Language Danish
+
+		SET @Aarstal = DATEPART(YEAR, @Dato)
+		
+		SET @Aar_Uge = DATEPART(YEAR, @ThursdayInWeek)
+
+		SET @Kvartal = DATEPART(QUARTER, @Dato) 
+		
+		SET @Kvartal_Uge = DATEPART(QUARTER, @ThursdayInWeek) 
+		
+		SET @Maaned = DATEPART(MONTH, @Dato)
+		
+        SET @Uge = DATEPART(ISO_WEEK, @Dato)
+
+		SET @Dag_Uge = DATEPART(DW, @Dato)
+
+		SET @Dag_Aar = DATEPART(DY, @Dato)
+		
+		SET @Dag_Maaned = DATEPART(DAY, @Dato)
+
+		SET @Maaned_Navn = DATENAME(MONTH, @Dato)
+		
+		SET @Dag_Navn = DATENAME (WEEKDAY, @Dato)
+
+        SET @DaysFromPalmSunday = DATEDIFF(DAY, @PalmSunday, @Dato)
+		
+        -- Sidste dagen i maaned.
+        IF DATEPART(DAY, DATEADD(DAY, 1, @Dato)) = 1
+            SET @IsLastDayInMonth = 1
+        ELSE
+            SET @IsLastDayInMonth = 0
+
+		-- Skudaar	
+        IF ((@Aarstal % 4 = 0) AND (@Aarstal % 100 != 0 OR @Aarstal % 400 = 0))
+			SET @Skudaar = 1
+		ELSE 
+			SET @Skudaar = 0
+
+		-- Insert værdier i tabel.
+		INSERT [Staging].[Dim_Tid] (		
+
+			 [Dato_Kode]
+			,[Dato]
+			,[Aarstal]
+			,[Aar_Uge]
+			,[Kvartal]
+			,[Kvartal_Uge]
+			,[Maaned]
+			,[Uge]
+			,[Dag_Uge] 
+			,[Dag_Aar] 
+			,[Dag_Maaned]
+			,[Maaned_Navn] 
+			,[Dag_Navn]   
+			,[Skudaar]
+		
+		 )
+		VALUES 
+		(
+			 @Dato_Kode
+			,@Dato
+			,@Aarstal
+			,@Aar_Uge
+			,@Kvartal
+			,@Kvartal_Uge
+			,@Maaned
+			,@Uge
+			,@Dag_Uge 
+			,@Dag_Aar 
+			,@Dag_Maaned
+			,@Maaned_Navn 
+			,@Dag_Navn   
+			,@Skudaar
+		)
+
+		-- Increment dato med et.
+		SELECT @Dato = DATEADD(DAY, 1, @Dato)
+
+	END
+	
+END
+GO
---
+++ Inspirations kode/sp_Staging_Adresse.sql
@@ -0,0 +1,240 @@
+﻿CREATE PROCEDURE [Staging].[sp_Staging_Adresse]
+     @StagingId BIGINT
+	,@LastSuccessfullJobId BIGINT = 0
+    ,@RecordsSelected BIGINT = 0
+	,@RecordsInserted BIGINT = 0
+	,@Status NVARCHAR (20) = 'Succeeded'
+	,@RecordsFailed INT = 0
+    ,@RecordsDiscarded INT = 0
+AS
+BEGIN TRY
+	BEGIN TRANSACTION
+
+--	 Delta update 
+UPDATE [Staging].[Adresse] 
+SET    
+       [ENHNR]                                     = AAA.[ENHNR]
+      ,[ADR_TYPE]								   = AAA.[ADR_TYPE]
+      ,[PE_PNR]									   = AAA.[PE_PNR]
+      ,[JE_CVRNR]								   = AAA.[JE_CVRNR]
+      ,[BP_BNR]									   = AAA.[BP_BNR]
+      ,[ASE_ANR]								   = AAA.[ASE_ANR]
+      ,[CPRVALID]								   = AAA.[CPRVALID]
+      ,[KM_KOM_ID]								   = AAA.[KM_KOM_ID]
+      ,[VA_VEJAFS_ID]							   = AAA.[VA_VEJAFS_ID]
+      ,[BY_VEJAFS_ID]							   = AAA.[BY_VEJAFS_ID]
+      ,[PVA_POST_VEJAFS_ID]						   = AAA.[PVA_POST_VEJAFS_ID]
+      ,[VEJKODE]								   = AAA.[VEJKODE]
+      ,[CO_NAVN]								   = AAA.[CO_NAVN]
+      ,[HUSNR_FRA]								   = AAA.[HUSNR_FRA]
+      ,[BOGST_FRA]								   = AAA.[BOGST_FRA]
+      ,[HUSNR_TIL]								   = AAA.[HUSNR_TIL]
+      ,[HUSNR]									   = CONCAT (AAA.HUSNR_FRA ,AAA.BOGST_FRA) 
+      ,[BOGST_TIL]								   = AAA.[BOGST_TIL]
+      ,[ETAGE]									   = AAA.[ETAGE]
+      ,[SIDE_DOER]								   = AAA.[SIDE_DOER]
+      ,[PD_POSTNR]								   = AAA.[PD_POSTNR]
+      ,[POSTBOKS]								   = AAA.[POSTBOKS]
+      ,[VEJNAVN_LANGT]							   = AAA.[VEJNAVN_LANGT]
+      ,[VEJNAVN_KORT]							   = AAA.[VEJNAVN_KORT]
+      ,[LD_ISO_LAND]							   = AAA.[LD_ISO_LAND]
+      ,[ADRESSE_FRITEKST]						   = AAA.[ADRESSE_FRITEKST]
+      ,[FAD_HEMMELIG_ADR]						   = AAA.[FAD_HEMMELIG_ADR]
+      ,[XADRESSE_FRITEKST]						   = AAA.[XADRESSE_FRITEKST]
+      ,[ADR_ART]								   = AAA.[ADR_ART]
+      ,[ENHEDSTYPE]								   = AAA.[ENHEDSTYPE]
+      ,[OPRET_DATO]								   = AAA.[OPRET_DATO]
+      ,[OPRET_BRUGER]							   = AAA.[OPRET_BRUGER]
+      ,[AJOUR_DATO]								   = AAA.[AJOUR_DATO]
+      ,[AJOUR_BRUGER]							   = AAA.[AJOUR_BRUGER]
+      ,[AJOUR_BATDATO]							   = AAA.[AJOUR_BATDATO]
+      ,[AJOUR_BATBRUGER]						   = AAA.[AJOUR_BATBRUGER]
+      ,[AJOUR_ANTAL]							   = AAA.[AJOUR_ANTAL]
+      ,[IABYG_INR]								   = AAA.[IABYG_INR]
+      ,[UDENLANDSK_POSTNUMMER]					   = AAA.[UDENLANDSK_POSTNUMMER]
+      ,[UDENLANDSK_BY]							   = AAA.[UDENLANDSK_BY]
+      ,[UDENLANDSK_POSTDISTRIKT]				   = AAA.[UDENLANDSK_POSTDISTRIKT]
+      ,[TCV_UDL_VIRKSOMHEDER_ID]				   = AAA.[TCV_UDL_VIRKSOMHEDER_ID]
+      ,[TCV_UDL_LOKATIONER_ID]					   = AAA.[TCV_UDL_LOKATIONER_ID]
+      ,[VEJNAVN_LANGT_UTF8]						   = AAA.[VEJNAVN_LANGT_UTF8]
+      ,[UDENLANDSK_POSTNUMMER_UTF8]				   = AAA.[UDENLANDSK_POSTNUMMER_UTF8]
+      ,[UDENLANDSK_POSTDISTRIKT_UTF8]			   = AAA.[UDENLANDSK_POSTDISTRIKT_UTF8]
+      ,[UDENLANDSK_BY_NAVN_UTF8]                   = AAA.[UDENLANDSK_BY_NAVN_UTF8]
+      ,[Meta_UpdateTime]                           = GETDATE()
+      ,[Meta_UpdateJob]	                           = @StagingId
+     
+FROM [RUT].[Archive].[ATIS_Adresser] AAA
+WHERE AAA.[ADRESSE_ID] = [Staging].[Adresse].[ADRESSE_ID]
+AND AAA.Meta_IsCurrent = 1 AND AAA.Meta_IsDeleted = 0
+AND AAA.Meta_CreateJob > @LastSuccessfullJobId
+
+    DECLARE @RecordsUpdated1 BIGINT 
+	SET @RecordsUpdated1 = (
+			SELECT @@ROWCOUNT
+			)
+
+---Delta insert
+	INSERT INTO [Staging].[Adresse] (
+	
+       [ADRESSE_ID]
+      ,[ENHNR]
+      ,[ADR_TYPE]
+      ,[PE_PNR]
+      ,[JE_CVRNR]
+      ,[BP_BNR]
+      ,[ASE_ANR]
+      ,[CPRVALID]
+      ,[KM_KOM_ID]
+      ,[VA_VEJAFS_ID]
+      ,[BY_VEJAFS_ID]
+      ,[PVA_POST_VEJAFS_ID]
+      ,[VEJKODE]
+      ,[CO_NAVN]
+      ,[HUSNR_FRA]
+      ,[BOGST_FRA]
+      ,[HUSNR_TIL]
+      ,[HUSNR]
+      ,[BOGST_TIL]
+      ,[ETAGE]
+      ,[SIDE_DOER]
+      ,[PD_POSTNR]
+      ,[POSTBOKS]
+      ,[VEJNAVN_LANGT]
+      ,[VEJNAVN_KORT]
+      ,[LD_ISO_LAND]
+      ,[ADRESSE_FRITEKST]
+      ,[FAD_HEMMELIG_ADR]
+      ,[XADRESSE_FRITEKST]
+      ,[ADR_ART]
+      ,[ENHEDSTYPE]
+      ,[OPRET_DATO]
+      ,[OPRET_BRUGER]
+      ,[AJOUR_DATO]
+      ,[AJOUR_BRUGER]
+      ,[AJOUR_BATDATO]
+      ,[AJOUR_BATBRUGER]
+      ,[AJOUR_ANTAL]
+      ,[IABYG_INR]
+      ,[UDENLANDSK_POSTNUMMER]
+      ,[UDENLANDSK_BY]
+      ,[UDENLANDSK_POSTDISTRIKT]
+      ,[TCV_UDL_VIRKSOMHEDER_ID]
+      ,[TCV_UDL_LOKATIONER_ID]
+      ,[VEJNAVN_LANGT_UTF8]
+      ,[UDENLANDSK_POSTNUMMER_UTF8]
+      ,[UDENLANDSK_POSTDISTRIKT_UTF8]
+      ,[UDENLANDSK_BY_NAVN_UTF8]
+      ,[Meta_CreateTime]
+      ,[Meta_CreateJob]
+      ,[Meta_UpdateTime]
+      ,[Meta_UpdateJob]
+      ,[Meta_DeleteTime]
+      ,[Meta_DeleteJob]
+		)
+	SELECT   
+		[ADRESSE_ID]
+      ,[ENHNR]
+      ,[ADR_TYPE]
+      ,[PE_PNR]
+      ,[JE_CVRNR]
+      ,[BP_BNR]
+      ,[ASE_ANR]
+      ,[CPRVALID]
+      ,[KM_KOM_ID]
+      ,[VA_VEJAFS_ID]
+      ,[BY_VEJAFS_ID]
+      ,[PVA_POST_VEJAFS_ID]
+      ,[VEJKODE]
+      ,[CO_NAVN]
+      ,[HUSNR_FRA]
+      ,[BOGST_FRA]
+      ,[HUSNR_TIL]
+	  ,CONCAT (
+		HUSNR_FRA
+		,BOGST_FRA
+		) AS HUSNR
+      ,[BOGST_TIL]
+      ,[ETAGE]
+      ,[SIDE_DOER]
+      ,[PD_POSTNR]
+      ,[POSTBOKS]
+      ,[VEJNAVN_LANGT]
+      ,[VEJNAVN_KORT]
+      ,[LD_ISO_LAND]
+      ,[ADRESSE_FRITEKST]
+      ,[FAD_HEMMELIG_ADR]
+      ,[XADRESSE_FRITEKST]
+      ,[ADR_ART]
+      ,[ENHEDSTYPE]
+      ,[OPRET_DATO]
+      ,[OPRET_BRUGER]
+      ,[AJOUR_DATO]
+      ,[AJOUR_BRUGER]
+      ,[AJOUR_BATDATO]
+      ,[AJOUR_BATBRUGER]
+      ,[AJOUR_ANTAL]
+      ,[IABYG_INR]
+      ,[UDENLANDSK_POSTNUMMER]
+      ,[UDENLANDSK_BY]
+      ,[UDENLANDSK_POSTDISTRIKT]
+      ,[TCV_UDL_VIRKSOMHEDER_ID]
+      ,[TCV_UDL_LOKATIONER_ID]
+      ,[VEJNAVN_LANGT_UTF8]
+      ,[UDENLANDSK_POSTNUMMER_UTF8]
+      ,[UDENLANDSK_POSTDISTRIKT_UTF8]
+      ,[UDENLANDSK_BY_NAVN_UTF8]
+      ,GETDATE()
+      ,@StagingId
+      ,NULL
+      ,NULL
+      ,NULL
+      ,NULL
+  FROM [RUT].[Archive].[ATIS_Adresser] AS AA
+	WHERE NOT EXISTS (
+			SELECT 1
+			FROM [Staging].[Adresse] AS SA
+			WHERE AA.[ADRESSE_ID] = SA.[ADRESSE_ID]
+			)
+	AND AA.Meta_IsCurrent = 1 AND AA.Meta_IsDeleted = 0
+	AND AA.Meta_CreateJob > @LastSuccessfullJobId
+
+	SET @RecordsSelected = (
+			SELECT @@ROWCOUNT
+			)
+
+
+--	 Delta Delete 
+UPDATE [Staging].[Adresse] 
+SET 
+       [Meta_DeleteTime]                           = GETDATE()
+      ,[Meta_DeleteJob]	                           = @StagingId
+
+FROM [RUT].[Archive].[ATIS_Adresser] AAA
+WHERE AAA.[ADRESSE_ID] = [Staging].[Adresse].[ADRESSE_ID]
+AND AAA.Meta_IsCurrent = 1 AND AAA.Meta_IsDeleted = 1
+AND AAA.Meta_DeleteJob > @LastSuccessfullJobId
+
+
+ DECLARE @RecordsUpdated2 BIGINT 
+	 	SET @RecordsUpdated2 = (
+			SELECT @@ROWCOUNT
+			)
+
+SET @RecordsInserted = @RecordsUpdated1 + @RecordsUpdated2 + @RecordsSelected
+
+
+	UPDATE LZDB.Audit.StagingLog
+	SET  [RecordsSelected]=@RecordsInserted
+		,[RecordsInserted] = @RecordsInserted
+		,[RecordsFailed] = @RecordsFailed
+		,[RecordsDiscarded] = @RecordsDiscarded
+		,[Status] = @Status
+        ,[EndTime] = GETDATE()
+	WHERE [Id] = @StagingId
+
+	COMMIT
+END TRY
+
+BEGIN CATCH
+	ROLLBACK TRANSACTION RAISEERROR
+END CATCH
