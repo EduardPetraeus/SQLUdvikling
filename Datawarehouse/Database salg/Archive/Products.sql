15b4462deaaf30581532535adde1ceb4f55e930b
eduardpetraeus <clauspetraeus@hotmail.com>
2020-05-14 16:39:19 +0200

    no message

---
+++ Datawarehouse/Database salg/Archive/Products.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [Archive].[Products]
+(
+    [Product_Id]               BIGINT                 NOT NULL,
+	[Product_Name]             VARCHAR(2000)          NULL,
+	[Aisle_Id]                 NVARCHAR(50)           NULL,
+	[Department_Id]            NVARCHAR(50)           NULL,
+
+	/* Metadata */               
+    [Meta_Id]                    BIGINT       NOT NULL IDENTITY (1, 1),
+    [Meta_VersionNo]             INT          NOT NULL,
+    [Meta_ValidFrom]             DATETIME     NOT NULL,
+    [Meta_ValidTo]               DATETIME     NULL,
+    [Meta_IsValid]               BIT          NOT NULL, -- If false rows exists in data validation table
+    [Meta_IsCurrent]             BIT          NOT NULL,
+    [Meta_IsDeleted]             BIT          NOT NULL,
+    [Meta_CreateTime]            DATETIME     NOT NULL,
+    [Meta_CreateJob]             BIGINT       NOT NULL, -- Reference to the audit framework
+    [Meta_UpdateTime]            DATETIME     NULL,
+    [Meta_UpdateJob]             BIGINT       NULL, -- Reference to the audit framework
+    [Meta_DeleteTime]            DATETIME     NULL,
+    [Meta_DeleteJob]             BIGINT       NULL, -- Reference to the audit framework
+          /* Constraints */
+	CONSTRAINT PK_Archive_Products PRIMARY KEY CLUSTERED ([Product_Id], [Meta_ValidFrom])
+);
+GO
+	
+CREATE UNIQUE INDEX IDX_Archive_Products ON [Archive].[Products] ([Meta_Id])
---
+++ Datawarehouse/Database salg/Extract/Products.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [Extract].[Products]
+(   
+    [Product_Id]               BIGINT                 NOT NULL,
+	[Product_Name]             VARCHAR(2000)          NULL,
+	[Aisle_Id]                 NVARCHAR(50)           NULL,
+	[Department_Id]            NVARCHAR(50)           NULL,
+
+	/* Metadata */
+	[Meta_Id]           BIGINT   IDENTITY (1,1)      NOT NULL,
+	[Meta_CreateTime]   DATETIME DEFAULT GETDATE()   NOT NULL,
+	[Meta_CreateJob]	BIGINT		                 NOT NULL
+
+	CONSTRAINT PK_Products_Id PRIMARY KEY CLUSTERED ([Meta_Id])
+)
+GO
+
+CREATE UNIQUE INDEX IDX_Products
+ON [Extract].[Products] ([Product_Id], [Meta_CreateTime])
