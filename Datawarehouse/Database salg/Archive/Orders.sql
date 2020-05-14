b2e761dc5aa6417750797925e3ba9c5189eafb86
eduardpetraeus <clauspetraeus@hotmail.com>
2020-05-14 10:28:44 +0200

    Ny tabel med SSIS pakker mv

---
+++ Datawarehouse/Database salg/Archive/Orders.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [Archive].[Orders]
+(
+    [Order_Id]                 BIGINT                 NOT NULL,
+	[User_Id]                  INT                    NULL,
+	[Eval_Set]                 NVARCHAR(50)           NULL,
+	[Order_Number]             INT                    NULL,
+	[Order_Dow]                INT                    NULL,
+	[Order_Hour_Of_Day]        INT                    NULL,
+	[Days_Since_Prior_Order]   NVARCHAR(50)           NULL,
+
+	/* Metadata */               
+    [Meta_Id]                    BIGINT       NOT NULL IDENTITY (1, 1),
+    [Meta_VersionNo]             INT          NOT NULL,
+    [Meta_ValidFrom]             DATETIME     NOT NULL,
+    [Meta_ValidTo]               DATETIME     NULL,
+    [Meta_IsValid]               BIT          NOT NULL, -- If false rows exists in data validation table
+    [Meta_IsCurrent]             BIT          NOT NULL,
+    [Meta_IsDeleted]             BIT          NOT NULL,
+    [Meta_CreateTime]            DATETIME     NOT NULL,
+    [Meta_CreateJob]             BIGINT       NOT NULL, -- Reference to the audit framework
+    [Meta_UpdateTime]            DATETIME     NULL,
+    [Meta_UpdateJob]             BIGINT       NULL, -- Reference to the audit framework
+    [Meta_DeleteTime]            DATETIME     NULL,
+    [Meta_DeleteJob]             BIGINT       NULL, -- Reference to the audit framework
+          /* Constraints */
+	CONSTRAINT PK_Archive_Orders PRIMARY KEY CLUSTERED ([Order_Id], [Meta_ValidFrom])
+);
+GO
+	
+CREATE UNIQUE INDEX IDX_Archive_Orders ON [Archive].[Orders] ([Meta_Id])
---
+++ Datawarehouse/Database salg/Extract/Orders.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [Extract].[Orders]
+(   
+    [Order_Id]                 BIGINT                 NOT NULL,
+	[User_Id]                  INT                    NULL,
+	[Eval_Set]                 NVARCHAR(50)           NULL,
+	[Order_Number]             INT                    NULL,
+	[Order_Dow]                INT                    NULL,
+	[Order_Hour_Of_Day]        INT                    NULL,
+	[Days_Since_Prior_Order]   NVARCHAR(50)           NULL,
+
+
+	/* Metadata */
+	[Meta_Id]           BIGINT   IDENTITY (1,1)      NOT NULL,
+	[Meta_CreateTime]   DATETIME DEFAULT GETDATE()   NOT NULL,
+	[Meta_CreateJob]	BIGINT		                 NOT NULL
+
+	CONSTRAINT PK_Orders_Id PRIMARY KEY CLUSTERED ([Meta_Id])
+)
+GO
+
+CREATE UNIQUE INDEX IDX_Orders
+ON [Extract].[Orders] ([Order_Id], [Meta_CreateTime])
