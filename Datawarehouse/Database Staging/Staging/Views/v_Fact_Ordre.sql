59fb240f983091f79f2927ea8e3fe11413e3d4d7
eduardpetraeus <clauspetraeus@hotmail.com>
2020-05-18 18:24:56 +0200

    no message

--- Datawarehouse/Database Staging/Staging/Views/v_Fact_Ordre.sql
+++ Datawarehouse/Database Staging/Staging/Views/v_Fact_Ordre.sql
@@ -1,6 +1,7 @@
 ﻿CREATE VIEW [Staging].[v_Fact_Ordre]
 	AS 
-SELECT SAO.[Order_Id] AS [Bkey_Dim_Kunde]
+SELECT TOP 5000000  -- Jeg tager kun 5 millioner rækker med over, fordi min lokale maskine har problemer med at køre flere rækker over uden at chrashe. Datasættet indeholder omkring 35 millioner rækker
+       SAO.[Order_Id] AS [Bkey_Dim_Kunde]
       ,SAO.[Order_Id]
       ,SAO.[Eval_Set]
       ,SAO.[Order_Number]
@@ -11,10 +12,14 @@ SELECT SAO.[Order_Id] AS [Bkey_Dim_Kunde]
       ,SOP.[Add_To_Cart_Order]
       ,SOP.[Reordered]
       ,LAE.[StartTime] AS [Initial_Load_Time]
-  FROM [Salg].[Extract].[Orders] SAO
+  FROM [Salg].[Archive].[Orders] SAO 
   LEFT JOIN [Staging].[Temp_Orders_Products] SOP
   ON SAO.[Order_Id] = SOP.[Order_Id]
 
   LEFT JOIN (SELECT MAX([StartTime]) AS [StartTime] FROM [LZDB].[Audit].[ExtractLog] 
   WHERE [TableName] = N'Orders' AND [Database] =N'Salg' and [Status] = N'Succeeded') LAE ON 1=1 
+
+  WHERE SAO.Meta_IsCurrent = 1 AND SAO.Meta_IsDeleted = 0
+ 
+ ORDER BY SAO.[Order_Id]
 
