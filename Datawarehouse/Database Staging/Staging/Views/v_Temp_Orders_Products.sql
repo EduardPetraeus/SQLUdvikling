7e0cbde5ee68b6e629e8a6a8b05acd1200768dd3
eduardpetraeus <clauspetraeus@hotmail.com>
2020-05-20 17:27:20 +0200

    no message

--- Datawarehouse/Database Staging/Staging/Views/v_Temp_Orders_Products.sql
+++ Datawarehouse/Database Staging/Staging/Views/v_Temp_Orders_Products.sql
@@ -4,7 +4,8 @@
       ,[Product_Id_Train] AS [Product_Id]
       ,[Add_To_Cart_Order_Train] AS [Add_To_Cart_Order]
       ,[Reordered_Train] AS [Reordered]
-  FROM [Salg].[Extract].[Order_Products__Train]
+  FROM [Salg].[Extract].[Order_Products__Train] -- Dette skal ideelt set være fra Archive tabellen, da denne indeholder Type2 historik, 
+                                                -- men min lokale computer har problemer med at køre 32 millioner rækker over i Archive tabellen med den bruge stored procedure
  
 
 UNION 
@@ -15,5 +16,6 @@ SELECT
       ,[Add_To_Cart_Order]
       ,[Reordered]
 
-  FROM [Salg].[Extract].[Order_Products__Prior]
+  FROM [Salg].[Extract].[Order_Products__Prior] -- Dette skal ideelt set være fra Archive tabellen, da denne indeholder Type2 historik, 
+                                                -- men min lokale computer har problemer med at køre 32 millioner rækker over i Archive tabellen med den bruge stored procedure
    
