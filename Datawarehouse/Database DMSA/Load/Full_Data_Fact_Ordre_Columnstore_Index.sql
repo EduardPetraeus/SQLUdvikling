5c5f72d515b7bc7ee69c2a28f59beae48319c262
Claus Eduard Petræus <cep@netcompany.com>
2021-02-10 15:00:15 +0100

    Datavarehus inkl Databaser, SSIS projekter og en SSAS kube

---
+++ Datawarehouse/Database DMSA/Load/Full_Data_Fact_Ordre_Columnstore_Index.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [Load].[Full_Data_Fact_Ordre_Columnstore_Index]
+(   [Bkey_Kunde]               INT                    NOT NULL,
+	[Order_Id]                 INT                    NOT NULL,
+	[Eval_Set]                 NVARCHAR(20)           NULL,
+	[Order_Number]             SMALLINT               NOT NULL,
+	[Order_Dow]                SMALLINT               NOT NULL,
+	[Order_Hour_Of_Day]        SMALLINT               NOT NULL,
+	[Days_Since_Prior_Order]   DECIMAL(6,1)           NULL,
+	[Bkey_Produkt]             INT                    NOT NULL,
+	[Add_To_Cart_Order]        SMALLINT               NOT NULL,
+	[Reordered]                SMALLINT               NOT NULL,
+	[Initial_Load_Time]        DATETIME2(7)           NULL,
+
+	
+	/* Metadata */
+	[Meta_CreateTime]   DATETIME DEFAULT GETDATE()   NOT NULL,
+	[Meta_CreateJob]	BIGINT		                 NOT NULL
+)
+GO 
+
+CREATE CLUSTERED COLUMNSTORE INDEX Load_Full_Data_Fact_Ordre_Columnstore_Index ON [Load].[Full_Data_Fact_Ordre_Columnstore_Index]
+
+GO
+
---
+++ Datawarehouse/Database DMSA/Staging/Views/v_Fact_Ordre.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [Staging].[v_Fact_Ordre]
+	AS 
+SELECT  
+       SAO.[Order_Id] AS [Bkey_Dim_Kunde]
+      ,SAO.[Order_Id]
+      ,SAO.[Eval_Set]
+      ,SAO.[Order_Number]
+      ,SAO.[Order_Dow]
+      ,SAO.[Order_Hour_Of_Day]
+      ,ISNULL(CAST(SAO.[Days_Since_Prior_Order] AS DECIMAL(6,1)),-1) AS [Days_Since_Prior_Order]
+	  ,ISNULL(SOP.[Product_Id],-1) AS [Bkey_Dim_Produkt]
+      ,ISNULL(SOP.[Add_To_Cart_Order],-1) AS [Add_To_Cart_Order]
+      ,ISNULL(SOP.[Reordered],-1) [Reordered]
+      ,LAE.[StartTime] AS [Initial_Load_Time]
+  FROM [Salg].[Archive].[Orders] SAO 
+  LEFT JOIN [Staging].[Staging].[Temp_Orders_Products] SOP
+  ON SAO.[Order_Id] = SOP.[Order_Id]
+
+  LEFT JOIN (SELECT MAX([StartTime]) AS [StartTime] FROM [LZDB].[Audit].[ExtractLog] 
+  WHERE [TableName] = N'Orders' AND [Database] =N'Salg' and [Status] = N'Succeeded') LAE ON 1=1 
+
+  WHERE SAO.Meta_IsCurrent = 1 AND SAO.Meta_IsDeleted = 0
+ 
+
