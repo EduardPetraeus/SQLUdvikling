32cc75c11e748cc2ac628dfc28e9ddb0b564dbf5
Claus Eduard Petræus <cep@netcompany.com>
2021-02-11 15:58:38 +0100

    Datavarehus inkl Databaser, SSIS projekter og en SSAS kube

---
+++ Datawarehouse/Database DZDB/Meta/Tables/VariblesForSSIS.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Meta].[VariblesForSSIS]
+(
+[Id]                   INT IDENTITY(1,1)           NOT NULL, 
+[SchemaTableName]      NVARCHAR(200)               NULL,
+[TableName]            NVARCHAR(200)               NULL, 
+[TruncateTable]        NVARCHAR(200)               NULL,
+[Year]                 SMALLINT	 	               NULL,
+[DateCreated]          DATETIME  DEFAULT GETDATE() NULL,
+CONSTRAINT PK_VariblesForSSIS_Id PRIMARY KEY CLUSTERED ([Id])
+) 
+GO
